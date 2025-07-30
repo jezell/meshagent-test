e8813978a4b8042ed427d254da3f260fc6c2bcb3
Jesse Ezell <jesse.ezell@timu.com>
2025-07-30 10:51:52 -0700

    turn on debug logging

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 system_prompt = os.getenv("SYSTEM_PROMPT")
 
@@ -14,14 +14,15 @@ print(f"starting service with rule: {system_prompt}", flush=True)
 
 host = ServiceHost()
 
+
 @host.path("/agent")
 class MyChatBot(ChatBot):
-  def __init__(self):
-    super().__init__(
-      name="chatbot",
-      rules=[ system_prompt ],
-      llm_adapter=OpenAIResponsesAdapter(),
-    )
+    def __init__(self):
+        super().__init__(
+            name="chatbot",
+            rules=[system_prompt],
+            llm_adapter=OpenAIResponsesAdapter(),
+        )
+
 
 asyncio.run(host.run())
-
